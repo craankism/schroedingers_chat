8fa1c0e7e2e0f1f66f831c4aa5a744b7783aed5c
craankism <saschaschwellnus@gmail.com>
2026-06-18 13:07:53 +0000

    feat: login view with 'create account' and 'forgot password' #35

---
+++ frontend/src/views/LoginView.tsx
@@ -0,0 +1,97 @@
+import type { JSX } from "@emotion/react/jsx-runtime";
+import {
+  Button,
+  Container,
+  Grid,
+  Link,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const LoginView = (): JSX.Element => {
+  const navigate = useNavigate();
+
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const submitHandler = (e: React.SubmitEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+    // placeholder
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <Container
+        sx={{
+          display: "flex",
+          minHeight: { xs: "calc(100vh - 68.5px)", md: "calc(100vh - 64px)" },
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Grid
+          container
+          sx={{
+            maxWidth: { xs: "90%", md: "30%" },
+            gap: 2,
+          }}
+        >
+          <Grid size={12}>
+            <Typography variant="h1">Login</Typography>
+          </Grid>
+          <Grid size={12}>
+            <TextField
+              id="username"
+              type="text"
+              label="Username"
+              variant="outlined"
+              fullWidth
+              defaultValue={username}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setUsername(e.target.value)
+              }
+            />
+          </Grid>
+          <Grid size={12}>
+            <TextField
+              id="password"
+              type="password"
+              label="Password"
+              variant="outlined"
+              fullWidth
+              defaultValue={password}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setPassword(e.target.value)
+              }
+            />
+          </Grid>
+          <Grid size={12}>
+            <Button type="submit" variant="contained">
+              LOGIN
+            </Button>
+          </Grid>
+          <Grid>
+            <Link
+              onClick={() => navigate("/code")}
+              underline="none"
+              sx={{ cursor: "pointer" }}
+            >
+              <Typography>Create Account</Typography>
+            </Link>
+            <Link
+              onClick={() => navigate("/placeholder")}
+              underline="none"
+              sx={{ cursor: "pointer" }}
+            >
+              <Typography>Forgot Password?</Typography>
+            </Link>
+          </Grid>
+        </Grid>
+      </Container>
+    </form>
+  );
+};
+
+export default LoginView;
