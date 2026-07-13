ac68c251aaf1640461598845043bb295ba43a0c6
Pascal Ender <pascal.ender@gmail.com>
2026-07-13 14:18:10 +0200

    fix: added sound to messages / AI message fixed frontend

--- frontend/src/components/views/Chat.tsx
+++ frontend/src/components/views/Chat.tsx
@@ -4,9 +4,11 @@ import React, { useCallback, useEffect, useRef, useState } from "react";
 import MessagesDisplay from "../main/chat/MessagesDisplay";
 import Message from "../main/chat/Message";
 import { heightMinusTopNav } from "../../types/constants/constants";
-import type { MessageType } from "../../types/MessageType";
+import type { MessageType} from "../../types/MessageType";
 import MemberSidebar from "../main/chat/MemberSidebar";
 import { useMessageStore } from "../../stores/MessageStore";
+import ICQSound from "../../sounds/ICQSound.mp3";
+import {decodeJwt} from "../../stores/AuthStore.ts";
 
 type ChatProps = {
   roomId: number;
@@ -40,7 +42,11 @@ const Chat: React.FC<ChatProps> = (roomId) => {
         client.subscribe(
           "/topic/" + roomId.roomId + "/messages",
           (incomingMessage) => {
-            setMessages(JSON.parse(incomingMessage.body));
+              const receivedMessage = JSON.parse(incomingMessage.body);
+              setMessages(receivedMessage);
+              if (receivedMessage.userId !== decodeJwt()?.userId) {
+                  new Audio(ICQSound).play();
+              }
           },
         );
 
