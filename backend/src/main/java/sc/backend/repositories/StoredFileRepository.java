ce52b0984b8f0f292930543d4456c73d76fd948d
Pascal Ender <pascal.ender@gmail.com>
2026-07-16 14:21:50 +0200

    fix: added back accidental line deletion

--- backend/src/main/java/sc/backend/repositories/StoredFileRepository.java
+++ backend/src/main/java/sc/backend/repositories/StoredFileRepository.java
@@ -5,7 +5,10 @@ import sc.backend.entities.Folder;
 import sc.backend.entities.StoredFile;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface StoredFileRepository extends JpaRepository<StoredFile, Integer> {
+    Optional<StoredFile> findFirstByFilenameIgnoreCase(String filename);
+
     List<StoredFile> findByFolder(Folder folder);
 }
