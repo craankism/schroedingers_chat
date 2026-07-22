a758c81e69444cd5d1bceee2b8b1f7c64aff5e91
craankism <saschaschwellnus@gmail.com>
2026-07-22 10:54:52 +0200

    chore: removed unused imports

--- backend/src/main/java/sc/backend/dtos/res/DocumentDTO.java
+++ backend/src/main/java/sc/backend/dtos/res/DocumentDTO.java
@@ -1,8 +1,6 @@
 package sc.backend.dtos.res;
 
 import lombok.*;
-import sc.backend.entities.DocumentMembership;
-import sc.backend.entities.User;
 
 import java.time.LocalDateTime;
 import java.util.List;
--- backend/src/main/java/sc/backend/repositories/DocumentRepository.java
+++ backend/src/main/java/sc/backend/repositories/DocumentRepository.java
@@ -3,7 +3,6 @@ package sc.backend.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import sc.backend.entities.Document;
 
-import java.util.Optional;
 
 public interface DocumentRepository extends JpaRepository<Document, Integer> {
 }
